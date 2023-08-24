28d3e1d97a5885c4ddcf66ecba9a78eaf17d102f
Abhay1509 <abhayjaiswal333333.aj@gmail.com>
2023-08-24 17:31:59 +0530

    logout_add

--- app_frontend/src/components/shared/LoggedInComponent.js
+++ app_frontend/src/components/shared/LoggedInComponent.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
+import React from "react";
 import VideoRecorder from "../../routes/VideoRecorder";
 import AudioRecorder from "../../routes/AudioRecorder";
+import { useCookies } from "react-cookie";
 
 const LoggedInComponent = () => {
   let [recordOption, setRecordOption] = useState("video");
+  const [cookie, , removeCookie] = useCookies(["token"]);
 
   const toggleRecordOption = (type) => {
     return () => {
       setRecordOption(type);
     };
+  };
+  const handleLogout = () => {
+    removeCookie("token");
+    window.location.href = "/login";
   };
 
   return (
@@ -17,6 +24,7 @@ const LoggedInComponent = () => {
       <div className="button-flex">
         <button onClick={toggleRecordOption("video")}>Record Video</button>
         <button onClick={toggleRecordOption("audio")}>Record Audio</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
       <div>
         {recordOption === "video" ? <VideoRecorder /> : <AudioRecorder />}
